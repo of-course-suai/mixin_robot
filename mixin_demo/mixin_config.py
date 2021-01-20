e90cad711ca6cc92f0a81aea90a2d54cec0b07cf
沙滩之子 <77472588+of-course-suai@users.noreply.github.com>
2021-01-20 09:30:24 +0800

    Update mixin_config.py

--- mixin_demo/mixin_config.py
+++ mixin_demo/mixin_config.py
@@ -5,31 +5,20 @@ get below config from 'https://developers.mixin.one/dashboard'
 code by lee.c
 update at 2018.12.2
 """
+# 这里是你的机器人参数
 
-client_id= '959f944f-603f-437c-a566-9acc1c0e070e'
+client_id= ''
 
-client_secret = '77528561c84bb956df5a50a5e498bb5092e1c496fdc892e8e8dd12869e7a845f'
+client_secret = ''
 
 
-pay_pin = '543555'
-pay_session_id = '510ced83-9a1f-4bb1-8464-390a0f58cb83'
-pin_token = "WCwpiKg58P+dHstdQWOEnbTnwewoIn4O2Y2Vf1itoTl+PhrCnuAn7C+HisjGcrF5slUVYdl7x5N3CEB65/7U+Y8i5ARjDsONciHvPx044GF0zbt1sRhohZFdNmGzcv3lhnmy4qLS1Ea5f97eiabrwrZDfuKxQwbeOriX33KOG04="
+pay_pin = ''
+pay_session_id = ''
+pin_token = ""
 
 
 private_key =  """-----BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQCVkX7oTnWJ5sO6YJha+xMMNPn3wXPC9OzW3z8uC77rQvSkWfrl
-QaHdloml2Oe6hqicLiqYgDw/DRmduqI4JUQcsP901bE6cp1fei6ByJv1dLV7i1kZ
-o9jSJtBIxCDSJfp7Tg8PBMdOMcbAYhDkxutu812NCvw9ohIj++yWW0m9zQIDAQAB
-AoGBAISSpHRxJwoGu7K18BWkuqt1QaPzmxJRWqTS2QONcbrCbGpJP3R6CmmJB5Uz
-f6L7vPRCj4sBD2WrzOrcBPHTRlzzhpi4tjJrvUaM/ogWUUIAWWhEyfkNFINPli2T
-in28saueKRaTweyu8W3uxgnLSZbfDGokyMtVOxa69bEnTFEBAkEA2A1l1fypUaFd
-X+dHtGKhcUip65iqTw5RPrJz/2kX75VoMTlzfJVWpQBeLSgpWGNFsEU1h/AvIP4n
-ACipoq/S4QJBALE5Joa2dzmcn3BFUXwpEuykyxMN3+XX7OcxfWW62cdXmMj+TpL0
-auKIpardpEoWzgbI3tFYgguvCu1F/bW/dG0CQFzYxR7Eye7urbX9Km92w3Jvi7ZX
-kTkQKhILlpX4j/TB/FjLG7o6Y1XdZpt5WutFHjm2RF769QX0ssX6s4vai4ECQCY3
-IKuj5QmzUF4xVGTlpoMI9hFhyIoTia5A6b1qEVP++587102xDutSdAboTGj9iefE
-oOebok9Kq5yGnzL12fECQQDUDi+GQsG8E+8VslpIf0XguCjWWStIhRCAL+bfhISJ
-uDmwrFXXFiTosp/rzGJYOU9JXGYM3Jio0SzKcmK7PVQm
+
 -----END RSA PRIVATE KEY-----"""
 
 
