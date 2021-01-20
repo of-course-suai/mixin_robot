b8b60b9b8249b79ad7b72f481f059a4b5f0f882b
沙滩之子 <77472588+of-course-suai@users.noreply.github.com>
2021-01-20 10:06:07 +0800

    Update torn.py

--- mixin_demo/torn.py
+++ mixin_demo/torn.py
@@ -8,7 +8,7 @@ import json
 from mixin_api import MIXIN_API
 import mixin_config
 
-# 这步具体参考官方开发者文档，我这里是用tornado写的，你也可以用其它方法来写，这里我就不详细阐述了
+# 这步具体参考官方开发者文档，我这里是用tornado写的，你也可以用其它方法来写，这里我就不详细阐述了，因为涉及国家机密了
 # 不会有人给了源码都不会用吧，不会吧不会吧~【doge】
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
